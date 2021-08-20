2b4b979ef6ceddc809046eddd783ecb4e1ba7d02
Jack Hubert <jphubert93@gmail.com>
2021-08-20 15:00:09 -0600

    beginning user story #1 => as a user, i can view major us cities located at the teleport api endpoint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount(){
+    fetch('https://api.teleport.org/api/', {
+      headers: {
+        'Content-Type': 'application/json'
+      }})
+      .then( response => response.json() )
+      .then( console.log )
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Welcome to the Teleport API</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
